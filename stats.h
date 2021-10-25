ee8039b150c4a26c6bca76bcfceca1e72a0c1cdd
fabstays <92723482+fabstays@users.noreply.github.com>
2021-10-25 10:15:59 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,12 +1,5 @@
 struct Stats compute_statistics(const float* numberset, int setlength);
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
-}
-
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
