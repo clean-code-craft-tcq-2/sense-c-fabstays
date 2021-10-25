409266d8839ec96edc0b99455c2adba4fd830813
fabstays <92723482+fabstays@users.noreply.github.com>
2021-10-25 16:56:02 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -8,3 +8,6 @@ struct Stats{
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
+extern struct Stats s;
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
